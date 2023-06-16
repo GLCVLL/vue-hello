b494d6c91e86144e77bdd43a85c957d6fe1884e0
GLCVLL <g.ioele96@outlook.it>
2023-06-16 17:52:03 +0200

    added title and image

---
+++ README.md
@@ -0,0 +1,3 @@
+Inizializziamo e montiamo un'app Vue.
+Mettiamo in pagina un h1 il cui contenuto testuale deve derivare dai data di Vue.
+BONUS: proviamo ad aggiungere un'immagine, derivante anch'essa dai dati di Vue 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 console.log('js ok');
 
 console.log('vue ok', Vue);
+
+
+// estrapolo il metodo createApp
+const {createApp} = Vue;
+
+// inizializzo l'app Vue
+const app = createApp({
+    data() {
+        return {
+            title: 'Ciao Vue è un piacere conoscerti!',
+            imageUrl: 'img/vue-image.webp',
+            logo: 'Logo Vue'
+        }
+    }
+});
+
+// la monto nell'elemento HTML radice
+app.mount('#root');
